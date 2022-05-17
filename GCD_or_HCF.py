97aef85b8370b7d77335261790c1e7c6e6545947
Aman2913 <96662657+Aman2913@users.noreply.github.com>
2022-05-17 13:50:14 +0530

    a,b=map(int,input().split()) if a>b:     a,b=b,a for i in range(1,a+1):     if a%i==0 and b%i==0:         hcf=i print(hcf)

--- GCD_or_HCF.py
+++ GCD_or_HCF.py
@@ -1,7 +1,8 @@
-x,y=map(int,input().split())
-if x>y:
-    x,y=y,x
-for i in range(1,x+1):
-    if x%i==0 and y%i==0:
+a,b=map(int,input().split())
+if a>b:
+    a,b=b,a
+for i in range(1,a+1):
+    if a%i==0 and b%i==0:
         hcf=i
 print(hcf)
+        
