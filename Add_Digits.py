b6148bfe721812a78d62ec8041320cee86e075bf
Aman2913 <96662657+Aman2913@users.noreply.github.com>
2022-07-08 21:37:52 +0530

    n=int(input()) while True:     s=0     while n>0:         p=n%10         s += p         n//=10     if len(str(s))==1:         print(s)         break     else:         n = s

---
+++ Add_Digits.py
@@ -0,0 +1,12 @@
+n=int(input())
+while True:
+    s=0
+    while n>0:
+        p=n%10
+        s += p
+        n//=10
+    if len(str(s))==1:
+        print(s)
+        break
+    else:
+        n = s
