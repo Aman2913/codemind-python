f6b75d02e12e4ba18963ce2f9045c38c4e62bca8
Aman2913 <96662657+Aman2913@users.noreply.github.com>
2022-07-12 12:43:20 +0530

    def gcd(a,b):     if a%b==0:         return b     else:         return gcd(b,a%b) a,b=map(int,input().split()) hcf=gcd(a,b) print(hcf)

--- GCD_or_HCF.py
+++ GCD_or_HCF.py
@@ -1,8 +1,8 @@
+def gcd(a,b):
+    if a%b==0:
+        return b
+    else:
+        return gcd(b,a%b)
 a,b=map(int,input().split())
-if a>b:
-    a,b=b,a
-for i in range(1,a+1):
-    if a%i==0 and b%i==0:
-        hcf=i
+hcf=gcd(a,b)
 print(hcf)
-        
