6ffa4b5df29dcea12af9f627aa9b4b13c0516f30
Aman2913 <96662657+Aman2913@users.noreply.github.com>
2022-07-16 02:37:42 +0530

    def palindrome(n):     rev=0     temp=n     while temp:         rev=rev*10+temp%10         temp//=10     if rev==n:         return True     return False n=int(input()) a=n+1 b=n-1 while a:     if palindrome(a):         break     a+=1 while b:     if palindrome(b):         break     b-=1 p=a-n q=n-b if p==q:     print(b,a) elif p>q:     print(b) else:     print(a)

--- Closest_Palindrome_.py
+++ Closest_Palindrome_.py
@@ -1,41 +1,28 @@
-def left_pal(x):
-    temp = 0
-    rev = 0
-    while 1:
-        if temp==0:
-            temp = x
-            rev = 0
+def palindrome(n):
+    rev=0
+    temp=n
+    while temp:
         rev=rev*10+temp%10
         temp//=10
-        if temp==0 and rev==x:
-            break
-        if temp==0 and rev!=x:
-            x-=1
-    return rev 
-        
-def right_pal(right):
-    rev = 0
-    temp = 0
-    while 1:
-        if temp==0:
-            temp = right
-            rev = 0
-        rev=rev*10+temp%10
-        temp//=10
-        if temp==0 and rev==right:
-            break
-        if temp==0 and rev!=right:
-            right+=1
-    return rev  
-            
-n = int(input())
-left = n-1
-l=left_pal(left)
-right = n+1
-r=right_pal(right)
-if r-n==n-l:
-    print(l,r)
-elif r-n>n-l:
-    print(l)
+    if rev==n:
+        return True
+    return False
+n=int(input())
+a=n+1
+b=n-1
+while a:
+    if palindrome(a):
+        break
+    a+=1
+while b:
+    if palindrome(b):
+        break
+    b-=1
+p=a-n
+q=n-b
+if p==q:
+    print(b,a)
+elif p>q:
+    print(b)
 else:
-    print(r)
+    print(a)
