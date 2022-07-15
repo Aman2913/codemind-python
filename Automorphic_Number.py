bd9d5b1cc8cf25c348bcfc6bc87c45c13f5eaf7b
Aman2913 <96662657+Aman2913@users.noreply.github.com>
2022-07-16 02:19:29 +0530

    n=int(input()) square=n**2 l=len(str(n)) m=square%pow(10,l) if m==n:     print('Automorphic Number') else:     print('Not an Automorphic Number')

--- Automorphic_Number.py
+++ Automorphic_Number.py
@@ -1,8 +1,8 @@
-num=int(input())
-square=num**2
-num_of_digits=len(str(num))
-last_digits= square%pow(10,num_of_digits)
-if last_digits==num:
+n=int(input())
+square=n**2
+l=len(str(n))
+m=square%pow(10,l)
+if m==n:
     print('Automorphic Number')
 else:
     print('Not an Automorphic Number')
