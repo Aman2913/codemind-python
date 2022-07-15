7fc933eafae16176696e1ecd3ecf9444176f1b3e
Aman2913 <96662657+Aman2913@users.noreply.github.com>
2022-07-16 03:11:11 +0530

    n=list(map(int,input().split())) if n[0]>n[1]:     p=n[0] else:     p=n[1] for i in range(1,p):     if n[0]%i==0 and n[1]%i==0:         hcf=i lcm=(n[0]*n[1])/hcf print(int(lcm))

--- LCM.py
+++ LCM.py
@@ -1,7 +1,10 @@
-a, b = map(int, input().split())
-i=1
-while 1:
-    if (a*i)%b==0:
-        print(a*i)
-        break
-    i+=1
+n=list(map(int,input().split()))
+if n[0]>n[1]:
+    p=n[0]
+else:
+    p=n[1]
+for i in range(1,p):
+    if n[0]%i==0 and n[1]%i==0:
+        hcf=i
+lcm=(n[0]*n[1])/hcf
+print(int(lcm))
