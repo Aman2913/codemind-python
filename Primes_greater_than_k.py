aca0f7cca6989ef6b90c5ded5a07af5b7df87fda
Aman2913 <96662657+Aman2913@users.noreply.github.com>
2022-07-16 12:50:09 +0530

    def prime(n):     if n==1:         return False     for i in range(2,int(n**.5)+1):         if n%i==0:             return False     return True n=int(input()) x=list(map(int,input().split())) k=int(input()) c=0 for i in range(n):     if prime(x[i]) and x[i]>=k:         c+=1 print(c)

---
+++ Primes_greater_than_k.py
@@ -0,0 +1,17 @@
+def prime(n):
+    if n==1:
+        return False
+    for i in range(2,int(n**.5)+1):
+        if n%i==0:
+            return False
+    return True
+n=int(input())
+x=list(map(int,input().split()))
+k=int(input())
+c=0
+for i in range(n):
+    if prime(x[i]) and x[i]>=k:
+        c+=1
+print(c)
+
+        
