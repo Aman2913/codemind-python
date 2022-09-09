7cf1addeed0ab847c1ecfe48a43680057c668bab
Aman2913 <96662657+Aman2913@users.noreply.github.com>
2022-09-09 09:48:17 +0530

    n = int(input()) sq_n=n*n rev=0 while n:     rev=rev*10+n%10     n//=10 rev_sq=rev*rev p=0 while rev_sq:     p=p*10+rev_sq%10     rev_sq//=10 if sq_n==p:     print(True) else:     print(False)

--- Adam_number.py
+++ Adam_number.py
@@ -1,15 +1,15 @@
 n = int(input())
-sq_n = n*n
-rev = 0
+sq_n=n*n
+rev=0
 while n:
     rev=rev*10+n%10
     n//=10
-sq_rev = rev*rev
-adam = 0
-while sq_rev:
-    adam=adam*10+sq_rev%10
-    sq_rev//=10
-if adam==sq_n:
+rev_sq=rev*rev
+p=0
+while rev_sq:
+    p=p*10+rev_sq%10
+    rev_sq//=10
+if sq_n==p:
     print(True)
 else:
     print(False)
