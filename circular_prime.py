f9cfbcf510513384e8b28dbbcbd87b40e7599e64
Aman2913 <96662657+Aman2913@users.noreply.github.com>
2022-09-09 09:54:00 +0530

    def prime(n):     if n==1:         return 0     for i in range(2,int(n**0.5)+1):         if n%i==0:             return 0     return 1 def pal(n):     rev=0     while n:         rev=rev*10+n%10         n=n//10     return rev n=int(input()) if prime(n) and prime(pal(n)):     print('circular prime') elif prime(n):     print('prime but not a circular prime') else:     print('not prime')

--- circular_prime.py
+++ circular_prime.py
@@ -1,22 +1,20 @@
 def prime(n):
-    c = 0
-    for i in range(2,n//2):
+    if n==1:
+        return 0
+    for i in range(2,int(n**0.5)+1):
         if n%i==0:
-            c+=1
-    if c==0:
-        return True
-    else:
-        return False
-def rev(n):
-    r = 0
+            return 0
+    return 1
+def pal(n):
+    rev=0
     while n:
-        r=r*10+n%10
-        n//=10
-    return r
-n = int(input())
-if prime(n) and prime(rev(n)):
-    print("circular prime")
-elif prime(n) or prime(rev(n)):
-    print("prime but not a circular prime")
+        rev=rev*10+n%10
+        n=n//10
+    return rev
+n=int(input())
+if prime(n) and prime(pal(n)):
+    print('circular prime')
+elif prime(n):
+    print('prime but not a circular prime')
 else:
-    print("not prime")
+    print('not prime')
