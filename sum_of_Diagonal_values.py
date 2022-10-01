5cd2166ff91ea8a515fdcbeac02d04016aa9b539
Aman2913 <96662657+Aman2913@users.noreply.github.com>
2022-10-01 10:45:44 +0530

    n,m=map(int,input().split()) mat=[] s=0 for i in range(n):     x=list(map(int,input().split()))     mat.append(x) for i in range(n):     for j in range(m):         if i==j:             s+=mat[i][j]         elif i+j==n-1:             s+=mat[i][j]
    print(s)

--- sum_of_Diagonal_values.py
+++ sum_of_Diagonal_values.py
@@ -1,16 +1,16 @@
 n,m=map(int,input().split())
-s=0
-p=0
 mat=[]
+s=0
 for i in range(n):
     x=list(map(int,input().split()))
     mat.append(x)
-# print(mat)
 for i in range(n):
     for j in range(m):
         if i==j:
             s+=mat[i][j]
         elif i+j==n-1:
-            p+=mat[i][j]
-print(s+p)
-    
+            s+=mat[i][j]
+            
+            
+            
+print(s)            
