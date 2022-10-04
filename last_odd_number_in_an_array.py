c78dcf0bd8d9c3f963d226d4de6d1d0d32817c23
Aman2913 <96662657+Aman2913@users.noreply.github.com>
2022-10-04 14:37:59 +0530

    n=int(input()) x=list(map(int,input().split())) a=x[::-1] for i in a:     if i%2==1:         print(i)         break

--- last_odd_number_in_an_array.py
+++ last_odd_number_in_an_array.py
@@ -1,6 +1,7 @@
-n = int(input())
-arr = list(map(int,input().split()))
-for i in range(n-1,-1,-1):
-    if arr[i]%2==1:
-        print(arr[i])
+n=int(input())
+x=list(map(int,input().split()))
+a=x[::-1]
+for i in a:
+    if i%2==1:
+        print(i)
         break
