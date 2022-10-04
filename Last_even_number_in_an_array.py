53252ef4b0afb7f963cda5e4f2fb871a3978caff
Aman2913 <96662657+Aman2913@users.noreply.github.com>
2022-10-04 16:30:17 +0530

    n=int(input()) x=list(map(int,input().split())) d=x[::-1] for i in d:     if i%2==0:         print(i)         break

--- Last_even_number_in_an_array.py
+++ Last_even_number_in_an_array.py
@@ -1,6 +1,7 @@
-n = int(input())
-arr = list(map(int,input().split()))
-for i in range(n-1,-1,-1):
-    if arr[i]%2==0:
-        print(arr[i])
+n=int(input())
+x=list(map(int,input().split()))
+d=x[::-1]
+for i in d:
+    if i%2==0:
+        print(i)
         break
