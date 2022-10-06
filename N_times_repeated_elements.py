b6270cefda7a7dd9a0c5ecf4b0f071482ee8532e
Aman2913 <96662657+Aman2913@users.noreply.github.com>
2022-10-06 17:23:28 +0530

    n = int(input()) x = list(map(int,input().split())) k=int(input()) temp = [] c=0 for i in x:     if x.count(i)==k and i not in temp:         temp.append(i)         print(i,end=" ") if len(temp)==0:     print(-1)

--- N_times_repeated_elements.py
+++ N_times_repeated_elements.py
@@ -1,10 +1,11 @@
-n=int(input())
-x=list(map(int,input().split()))
+n = int(input())
+x = list(map(int,input().split()))
 k=int(input())
-m=[]
+temp = []
+c=0
 for i in x:
-    if x.count(i)==k and i not in m:
-        m.append(i)
-        print(i,end=' ')
-if len(m)==0:
+    if x.count(i)==k and i not in temp:
+        temp.append(i)
+        print(i,end=" ")
+if len(temp)==0:
     print(-1)
