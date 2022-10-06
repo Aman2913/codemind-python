b2caecae73c47721eb990be07df6396fedc2ab65
Aman2913 <96662657+Aman2913@users.noreply.github.com>
2022-10-06 18:18:57 +0530

    n,m=map(int,input().split()) n1=list(map(int,input().split())) m1=list(map(int,input().split())) temp=[] c=0 for i in n1:     if (i in m1) and (i not in temp):         temp.append(i)         c+=1 print(c)

--- Count_elements_I.py
+++ Count_elements_I.py
@@ -1,6 +1,10 @@
 n,m=map(int,input().split())
+n1=list(map(int,input().split()))
+m1=list(map(int,input().split()))
+temp=[]
 c=0
-x=set(list(map(int,input().split())))
-y=set(list(map(int,input().split())))
-p=x&y
-print(len(p))
+for i in n1:
+    if (i in m1) and (i not in temp): 
+        temp.append(i)
+        c+=1
+print(c)        
