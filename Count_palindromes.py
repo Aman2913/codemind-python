abf832d6f3ccf7c74dd7a1dfd303627c776b59a7
Aman2913 <96662657+Aman2913@users.noreply.github.com>
2022-10-06 18:28:58 +0530

    def palin(i):     s = str(i)     m = s[::-1]     if m==s:         return 1 n = int(input()) a = list(map(int,input().split())) c=0 for i in a:     if palin(i):         c+=1 print(c)

--- Count_palindromes.py
+++ Count_palindromes.py
@@ -1,12 +1,12 @@
-n=int(input())
+def palin(i):
+    s = str(i)
+    m = s[::-1]
+    if m==s:
+        return 1
+n = int(input())
+a = list(map(int,input().split()))
 c=0
-x=list(map(int,input().split()))
-for i in range(n):
-    temp=x[i]
-    rev=0
-    while temp:
-        rev=rev*10+temp%10
-        temp//=10
-    if rev==x[i]:
+for i in a:
+    if palin(i):
         c+=1
 print(c)
