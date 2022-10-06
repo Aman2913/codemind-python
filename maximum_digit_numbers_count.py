06f94a5ad9c1435636bfdd23b3890eccf709efc4
Aman2913 <96662657+Aman2913@users.noreply.github.com>
2022-10-06 18:30:38 +0530

    def dig(n):     if n==0:         return 1     c=0     if n<0:         n=-1*n     while n:         n=n//10         c+=1     return c n=int(input()) a=list(map(int,input().split())) l=[] for i in range(n):     l.append(dig(a[i])) for i in range(n):     if dig(a[i])==max(l):         print(a[i],end=' ')

--- maximum_digit_numbers_count.py
+++ maximum_digit_numbers_count.py
@@ -1,19 +1,18 @@
-def ln(n):
+def dig(n):
     if n==0:
         return 1
-    if n<0:
-        n*=-1
     c=0
+    if n<0:
+        n=-1*n
     while n:
+        n=n//10
         c+=1
-        n//=10
     return c
 n=int(input())
-x=list(map(int,input().split()))
-mc=0
-for i in x:
-    if ln(i)>mc:
-        mc=ln(i)
-for i in x:
-    if ln(i)==mc:
-        print(i,end=" ")
+a=list(map(int,input().split()))
+l=[]
+for i in range(n):
+    l.append(dig(a[i]))
+for i in range(n):
+    if dig(a[i])==max(l):
+        print(a[i],end=' ')
